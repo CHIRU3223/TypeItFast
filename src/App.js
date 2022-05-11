b7134b87411d1009a25c2bb7532718f23abcbbd4
chimadir <chiranjeevishriramkrishna.madired@publicissapient.com>
2022-05-11 20:05:03 +0530

    Init Run with No UI

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import { Home } from './components/Home';
+import { TypeBox } from './components/TypeBox';
+
+function App() {
+  return (
+    <div className="App">
+      <Home/>
+      <TypeBox/>
+    </div>
+  );
+}
+
+export default App;
